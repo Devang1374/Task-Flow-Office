4723f7f19fff4e9f2829339b8cf65fd13cb9361f
devang1374 <vagheladevang123@gmail.com>
2026-06-16 13:07:20 +0530

    Migration

--- database/migrations/2026_06_02_122258_create_roles_users_table.php
+++ database/migrations/2026_06_02_122258_create_roles_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles_users', function (Blueprint $table) {
             $table->id();
-            $table->integer('users_id');
+            $table->integer('user_id');
             $table->integer('roles_id');
             $table->timestamps();
         });
